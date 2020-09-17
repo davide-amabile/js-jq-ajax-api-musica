2619b5d47ff4d033d0507a840c751aca27b270ae
davide-amabile <davideamabile81@gmail.com>
2020-09-17 15:50:11 +0200

    handlebars

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-	
+  $.ajax(
+      {
+      "url": "https://flynn.boolean.careers/exercises/api/array/music",
+      "method": "GET",
+      "success": function (data, stato) {
+        console.log(data.response);
+        listaDischi(data.response);
+      },
+      "error": function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+      }
+      }
+      );
+
+      // creare la funzione del template
+      function listaDischi(dato) {
+      
+
+        for ( i = 0; i < dato.length; i++) {
+          var source = $("#dischi-template").html;
+          var template = Handlebars.compile(source);
+
+          var disco = dato[i];
+          var html = template(disco);
+
+          $(".cds-container").append(html);
+      }
+    }
 });
