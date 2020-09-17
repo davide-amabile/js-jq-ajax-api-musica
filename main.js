2a7d55062574f6354d4ec7365043bd81a29fe4c2
davide-amabile <davideamabile81@gmail.com>
2020-09-17 15:57:16 +0200

    template

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
 
       // creare la funzione del template
       function listaDischi(dato) {
-      
+
 
         for ( i = 0; i < dato.length; i++) {
-          var source = $("#dischi-template").html;
+          var source = $("#dischi-template").html();
           var template = Handlebars.compile(source);
 
           var disco = dato[i];
